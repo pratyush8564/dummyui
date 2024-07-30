776b1e4226cfb0a4ee49e5e8bcee37d50bed8665
Saawan123 <kumarsaawan@seasia.in>
2024-07-31 02:15:21 +0530

    highcharts

--- src/components/ChartCard.tsx
+++ src/components/ChartCard.tsx
@@ -3,12 +3,12 @@ import * as Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
 const ChartCard = ({ title, data, total, percentageChange, icon, dots }: any) => {
-    const chartData = data?.map((point: any) => [point.x, point.y]);
- 
+    const chartData = data?.map((point: any) => [point.x]);
+
 
     const options = {
         chart: {
-            type: 'line', 
+            type: 'line',
             height: 80,
             width: 100,
         },
@@ -16,29 +16,32 @@ const ChartCard = ({ title, data, total, percentageChange, icon, dots }: any) =>
             text: ''
         },
         xAxis: {
-        
+
             labels: {
                 enabled: false
             },
             tickWidth: 0,
-                    },
+        },
         yAxis: {
             title: {
-                text: ''            },
-            labels: {
-                enabled: false 
+                text: ''
             },
-        
-         
-            
+            labels: {
+                enabled: false
+            },
+            gridLineWidth: 0,
+
+
+
+
         },
         series: [
             {
                 data: chartData,
-                lineWidth: 1, 
-                color: '#08736D', 
+                lineWidth: 1,
+                color: '#08736D',
                 marker: {
-                    enabled: false 
+                    enabled: false
                 }
             }
         ],
@@ -49,11 +52,11 @@ const ChartCard = ({ title, data, total, percentageChange, icon, dots }: any) =>
             enabled: false
         }
     };
- 
+
     return (
         <div className="flex flex-col bg-white rounded-lg gap-4 p-4 flex-1 min-w-[170px] max-w-[calc(25%-16px)]">
             <div className="flex gap-2 items-center">
-             {dots}   <p className="font-medium">{title}</p>
+                {dots}   <p className="font-medium">{title}</p>
             </div>
             <div className="flex gap-2 mt-2 items-center">
                 <p className="text-2xl font-bold">{total}</p>
